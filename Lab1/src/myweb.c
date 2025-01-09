1d0c5acbf8e2cce079ae69a288cc2e3248e70d28
Calvin Owen <calvin.owen27@gmail.com>
2025-01-09 14:56:47 -0800

    doc path parsing working for my one little test

--- Lab1/src/myweb.c
+++ Lab1/src/myweb.c
@@ -8,62 +8,48 @@
 #include <string.h>
 #include <regex.h>
 #include "utils.h"
+#include "myweb.h"
 
 #define EMPTY_LINE_REGEX "\r\n"
 #define DOUBLE_EMPTY_LINE_REGEX EMPTY_LINE_REGEX EMPTY_LINE_REGEX
 #define CLH_REGEX "(Content-Length): ([ -~]{1,128})" EMPTY_LINE_REGEX
+#define IP_REGEX "([[0-9]{1,3}.]{3}[0-9]{1,3})"
+#define PORT_REGEX "[[:]{0,1}([0-9]{1,5})]{0,1}"
+#define PATH_REGEX "(/[ -~]+)"
+#define DOC_ARG_REGEX IP_REGEX PORT_REGEX PATH_REGEX
+
 #define BUFFER_SIZE 8196
 
-// prints correct program usage to stderr
-void usage(char *exec) {
-    fprintf(stderr,
-        "SYNOPSIS\n"
-        "Makes a simple GET request and download for a file from a specified url and IPv4 address.\n"
-		"Outputs requested file contents to output.dat\n"
-        "\n"
-        "USAGE\n"
-        "   %s [url] [IPv4]:[optional receiving port]/[desired file name] [-h]\n"
-        "\n"
-        "OPTIONS\n"
-        "   -h               prints server response headers to stdout, and does not receive document\n",
-        exec);
-}
-
-// initialize socket with ip address and port, and return the file descriptor for the socket
-// returns -1 on failure
-int init_socket(const char *ip_addr, int port) {
-	// initialize socket fd
-	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		fprintf(stderr, "init_socket(): failed to initialize socket\n");
-		return -1;
-	}
-
-	// init socket address struct with ip and port
-	struct sockaddr_in sockaddr;
-
-	sockaddr.sin_family = AF_INET; // IPv4
-	sockaddr.sin_port = htons(port); // convert port endianness
-	if (inet_pton(AF_INET, ip_addr, &sockaddr.sin_addr.s_addr) < 0) {
-		fprintf(stderr, "init_socket(): invalid ip address provided\n");
-		return -1;
-	}
-
-	// connect socket
-	if (connect(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
-		fprintf(stderr, "init_socket(): failed to connect socket\n");
-		return -1;
-	}
-
-	return sockfd;
-}
+struct doc_data {
+	char *ip_addr;
+	int port;
+	char *doc_path;
+};
 
 int main(int argc, char **argv) {
 	// parse command line args
 
-	for (int i = 1; i < argc; i++) {
-		printf("%s\n", argv[i]);
+	int header_opt = argc < 4 ? 0 : !strcmp(argv[3], "-h");
+
+	// wrong number of argc or extra option is not -h
+	if (argc < 3 || argc > 4 || (argc == 4 && !header_opt)) {
+		usage(argv[0]);
+		exit(1);
 	}
+
+	char *url_arg = argv[1];
+	char *doc_arg = argv[2];
+
+	printf("url: %s\n", url_arg);
+	printf("doc arg: %s\n", doc_arg);
+	printf("header option?: %d\n", header_opt);
+
+	struct doc_data data;
+
+	parse_path(&data, doc_arg);
+
+	free(data.ip_addr);
+	free(data.doc_path);
 
 	return 0;
 
@@ -120,4 +106,145 @@ int main(int argc, char **argv) {
 	close(sockfd);
 
 	return 0;
+}
+
+// initialize socket with ip address and port, and return the file descriptor for the socket
+// returns -1 on failure
+int init_socket(const char *ip_addr, int port) {
+	// initialize socket fd
+	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		fprintf(stderr, "init_socket(): failed to initialize socket\n");
+		return -1;
+	}
+
+	// init socket address struct with ip and port
+	struct sockaddr_in sockaddr;
+
+	sockaddr.sin_family = AF_INET; // IPv4
+	sockaddr.sin_port = htons(port); // convert port endianness
+	if (inet_pton(AF_INET, ip_addr, &sockaddr.sin_addr.s_addr) < 0) {
+		fprintf(stderr, "init_socket(): invalid ip address provided\n");
+		return -1;
+	}
+
+	// connect socket
+	if (connect(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
+		fprintf(stderr, "init_socket(): failed to connect socket\n");
+		return -1;
+	}
+
+	return sockfd;
+}
+
+// parse document path/ip address field into doc_data struct for further use
+// returns 0 on success, -1 on error
+int parse_path(struct doc_data *data, const char *path) {
+	data->port = -1;
+
+	// regex for parsing path arg
+	regex_t regex_;
+	// int res = regcomp(&regex_, DOC_ARG_REGEX, REG_EXTENDED);
+	// if (res < 0) {
+	// 	fprintf(stderr, "parse_path(): regcomp() failed\n");
+	// 	return data;
+	// }
+
+	// regmatch_t pmatch[4];
+	// res = regexec(&regex_, path, 4, pmatch, 0);
+	// if (res < 0) {
+	// 	if (res != REG_NOMATCH) {
+	// 		fprintf(stderr, "parse_path(): regexec() failed\n");
+	// 		return data;
+	// 	}
+	// 	fprintf(stderr, "parse_path(): no regex match found for doc path arg. Invalid IP/Port/document path given.\n");
+	// 	return data;
+	// }
+
+	// data.ip_addr = calloc(pmatch[1].rm_eo - pmatch[1].rm_so, sizeof(char));
+	// memcpy(data.ip_addr, path + pmatch[1].rm_so, sizeof(data.ip_addr));
+
+	// char ip_buf[pmatch[2].rm_eo - pmatch[2].rm_so + 1];
+	// ip_buf[sizeof(ip_buf) - 1] = 0;
+	// memcpy(ip_buf, path + pmatch[2].rm_so, sizeof(ip_buf));
+
+	// data.port = atoi(ip_buf);
+
+	// regex for separating ip/port and doc path
+	int res = regcomp(&regex_, "/", 0);
+	if (res < 0) {
+		fprintf(stderr, "parse_path(): regcomp() failed\n");
+		return -1;
+	}
+
+	regmatch_t pmatch[2];
+
+	res = regexec(&regex_, path, 1, &pmatch[1], 0);
+	if (res < 0 && res != REG_NOMATCH) {
+		fprintf(stderr, "parse_path(): regexec() failed\n");
+		return -1;
+	}
+
+	// allocate and transfer data from path string to ip address field
+	int doc_path_len = strlen(path) - pmatch[1].rm_so;
+	data->doc_path = calloc(doc_path_len + 1, sizeof(char));
+	data->doc_path[doc_path_len] = 0;
+	memcpy(data->doc_path, path + pmatch[1].rm_so, doc_path_len);
+	
+
+	// regex for separating ip and port
+	res = regcomp(&regex_, ":", 0);
+	if (res < 0) {
+		fprintf(stderr, "parse_path(): regcomp() failed\n");
+		return -1;
+	}
+
+	res = regexec(&regex_, path, 1, &pmatch[0], 0);
+	if (res < 0 && res != REG_NOMATCH) {
+		fprintf(stderr, "parse_path(): regexec() failed\n");
+		return -1;
+	}
+
+	int ip_addr_len;
+	if (res == REG_NOMATCH) {
+		ip_addr_len = pmatch[1].rm_so;
+	} else {
+		ip_addr_len = pmatch[0].rm_eo - 1;
+
+		int port_buf_len = pmatch[1].rm_so - pmatch[0].rm_eo;
+		char *port_buf = calloc(port_buf_len + 1, sizeof(char));
+		memcpy(port_buf, path + pmatch[0].rm_eo, port_buf_len);
+		data->port = atoi(port_buf);
+		if (data->port <= 0) {
+			fprintf(stderr, "parse_path(): invalid port specified\n");
+			return -1;
+		}
+		free(port_buf);
+	}
+
+	// int ip_addr_len = pmatch[1].rm_eo - pmatch[1].rm_so;
+	data->ip_addr = calloc(ip_addr_len + 1, sizeof(char));
+	data->ip_addr[ip_addr_len] = 0;
+	memcpy(data->ip_addr, path, ip_addr_len);
+
+	printf("IP: %s\n", data->ip_addr);
+	printf("Port: %d\n", data->port);
+	printf("Doc path: %s\n", data->doc_path);
+
+	return 0;
+}
+
+// prints correct program usage to stderr
+void usage(char *exec) {
+    fprintf(stderr,
+        "SYNOPSIS\n"
+        "Makes a simple GET request and download for a file from a specified url and IPv4 address.\n"
+		"/Outputs requested file contents to output.dat\n"
+        "\n"
+        "USAGE\n"
+        "   %s [url] [IPv4]:[optional receiving port]/[desired file name] [-h]\n"
+        "\n"
+        "OPTIONS\n"
+        "   -h               prints server response headers to stdout, and does not receive document\n",
+        exec);
 }
--- Lab1/src/myweb.h
+++ Lab1/src/myweb.h
@@ -1,6 +1,13 @@
+// struct for storing ip and document data after parsing
+struct doc_data;
+
 // prints correct program usage to stderr
 void usage(char *exec);
 
 // initialize socket with ip address and port, and return the file descriptor for the socket
 // returns -1 on failure
 int init_socket(const char *ip_addr, int port);
+
+// parse document path/ip address field into doc_data struct for further use
+// returns 0 on success, -1 on error
+int parse_path(struct doc_data *data, const char *path);
