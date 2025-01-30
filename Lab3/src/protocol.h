c96b37784e299b69f2470dcaa7e28f2cd5bf5a7f
Calvin Owen <calvin.owen27@gmail.com>
2025-01-29 17:33:29 -0800

    header constants defined

--- Lab3/src/protocol.h
+++ Lab3/src/protocol.h
@@ -1,7 +1,8 @@
 #define TIMEOUT_SECS 30
-#define SN_BYTES 4
-#define CID_BYTES 4
-#define PD_SZ_BYTES 4
+#define SN_BYTES 4			// num bytes for sequence number
+#define CID_BYTES 4			// num bytes for client ID
+#define PYLD_SZ_BYTES 4		// num bytes for payload size
+#define MAX_HEADER_SIZE CID_BYTES + SN_BYTES + PYLD_SZ_BYTES
 
 enum OPCODES {
 				WR = 1,		// write request
