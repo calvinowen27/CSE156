18081ca626138b6938b8d6dbd84dc1665303aa81
Calvin Owen <calvin.owen27@gmail.com>
2025-02-06 02:59:26 -0800

    fixing stupid integer arithmetic thing

--- Lab3/src/myclient.c
+++ Lab3/src/myclient.c
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
 	char *outfile_path = argv[6];															// outfile path
 
 	if (strlen(outfile_path) > (size_t)(mss - MAX_HEADER_SIZE) - 1) {
-		printf("MSS argument is too small for desired output file path. MSS value specified is %d bytes and header length is %d bytes. Please specify an outfile path that is less than or equal to %d - %d - 1 = %d bytes long, or provide a larger MSS\n", mss, WR_HEADER_SIZE, mss, WR_HEADER_SIZE, ((int)mss - (int)WR_HEADER_SIZE) - 1);
+		printf("MSS argument is too small for desired output file path. MSS value specified is %d bytes and header length is %d bytes. Please specify an outfile path that is less than or equal to %d - %d - 1 = %d bytes long, or provide a larger MSS\n", mss, WR_HEADER_SIZE, mss, WR_HEADER_SIZE, mss - (WR_HEADER_SIZE + 1));
 		exit(1);
 	}
 	
