3f42662b3dc55b2fc4ce8b66d5b5cf26e57ca4eb
Calvin Owen <calvin.owen27@gmail.com>
2025-02-06 23:19:30 -0800

    tests

--- Lab3/test/test_server_crash.sh
+++ Lab3/test/test_server_crash.sh
@@ -4,29 +4,27 @@ echo "
 !!! RUNNING TEST_SERVER_CRASH !!!
 "
 
-make crashingserver
-
 mkdir -p out
 
-./bin/crashingserver 9090 &
+./bin/myserver 9090 3 &
 server_pid=$!
 
-./bin/myclient 127.0.0.1 9090 5 test_files/small_ascii.txt out/small_ascii_out.txt > out/out 2> out/err &
+./bin/myclient 127.0.0.1 9090 1024 20 test_files/large_binary.dat out/large_binary_out.dat &
 client_pid=$!
 
+kill -9 $server_pid
 wait $client_pid
 
-if grep -q "Cannot detect server." out/err; then
-  echo "~~~~~~~~~~~~~~~~~~~~~~~
+if [ $? -eq 4 ]; then
+	echo "~~~~~~~~~~~~~~~~~~~~~~~
 	TEST SUCCESS
 ~~~~~~~~~~~~~~~~~~~~~~~"
 else
-	echo "~~~~~~~~~~~~~~~~~~~~~~~
-	TEST FAILURE: client didn't respond correctly to server crash
+echo "~~~~~~~~~~~~~~~~~~~~~~~
+	TEST FAILURE: clent did not terminate when server crashed
 ~~~~~~~~~~~~~~~~~~~~~~~"
 fi
 
-kill -9 $server_pid
 wait $server_pid &>/dev/null
 
 rm -rf out/
